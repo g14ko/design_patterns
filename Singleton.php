4f1c54813f2a3c3a5e496f352a46ef365b9b34ad
Eugene <g14ko@yandex.ru>
2014-10-19 14:38:37 +0300

    Add design pattern - Singleton.

---
+++ Singleton.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * User: Eugene Gichko
+ * Date: 19.10.14
+ * Time: 14:28
+ */
+
+class Singleton
+{
+
+    private static $instance;
+
+    /**
+     * Возвращает экземляр объекта данного класса.
+     *
+     * @return Singleton Объект данного класса.
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Запрещаем создание объекта вне класса
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * Запрещаем клонирование объекта
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Запрещаем десериализацию объекта
+     *
+     * @return void
+     */
+    private function __wakeup()
+    {
+    }
+
+}
